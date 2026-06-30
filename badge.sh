50768fb7157440c367574b53199c1e711d31a9f2
signalwerk <sh@signalwerk.ch>
2026-06-30 12:46:10 +0200

    MOD: update batch processing

--- badge.sh
+++ badge.sh
@@ -3,6 +3,7 @@ node src/cli.js \
     --format name-addition-image \
     --layout team-ensemble-badge-90x135-face \
     --image-dir "/Users/signalwerk/kDrive/Common documents/1 Team Ensemble/Missionen und Interventionen/Kollaborationsraum Demografie/Kick-Off 1. Juli/LinkedIn_Fotos/Teilnehmende/_ai-younger" \
+    --empty 2 \
     --output "/Users/signalwerk/kDrive/Common documents/1 Team Ensemble/Missionen und Interventionen/Kollaborationsraum Demografie/Kick-Off 1. Juli/LinkedIn_Fotos/Teilnehmende/_badges/badges.pdf"
 
 node src/cli.js \
@@ -10,5 +11,5 @@ node src/cli.js \
     --format name-addition-image \
     --layout team-ensemble-badge-90x135-face \
     --image-dir "/Users/signalwerk/kDrive/Common documents/1 Team Ensemble/Missionen und Interventionen/Kollaborationsraum Demografie/Kick-Off 1. Juli/LinkedIn_Fotos/Team Ensemble Fotos/_ai-younger" \
-    --empty 5 \
+    --empty 8 \
     --output "/Users/signalwerk/kDrive/Common documents/1 Team Ensemble/Missionen und Interventionen/Kollaborationsraum Demografie/Kick-Off 1. Juli/LinkedIn_Fotos/Team Ensemble Fotos/_badges/badges.pdf"
